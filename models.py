52062dcbc045ef029ce9a8ec5ee0ca80720bc01b
Melissa Avery-Weir <melissa.avery@wellsfargo.com>
2012-02-27 09:52:13 -0500

    All base traits entered.
    Signed-off-by: Melissa Avery-Weir <melissa.avery@wellsfargo.com>

--- models.py
+++ models.py
@@ -42,43 +42,7 @@ class Game(models.Model):
   
   class Meta:
     abstract = True
-    
- 
-  # def setup_skills(self):  
-    # this.Skills = [
-      # Trait(name = "Academics", trait_type = self.trait_types[1], category = Trait.category_options.mental, uses_simple_calculation = false),
-      # Trait(name = "Computer", trait_type = self.trait_types[1], category = Trait.category_options.mental, uses_simple_calculation = false),
-      # Trait(name = "Crafts", trait_type = self.trait_types[1], category = Trait.category_options.mental, uses_simple_calculation = false),
-      # Trait(name = "Investigation", trait_type = self.trait_types[1], category = Trait.category_options.mental, uses_simple_calculation = false),
-      # Trait(name = "Medicine", trait_type = self.trait_types[1], category = Trait.category_options.mental, uses_simple_calculation = false),
-      # Trait(name = "Occult", trait_type = self.trait_types[1], category = Trait.category_options.mental, uses_simple_calculation = false),
-      # Trait(name = "Politics", trait_type = self.trait_types[1], category = Trait.category_options.mental, uses_simple_calculation = false),
-      # Trait(name = "Science", trait_type = self.trait_types[1], category = Trait.category_options.mental, uses_simple_calculation = false),
-      # Trait(name = "Athletics", trait_type = self.trait_types[1], category = Trait.category_options.physical, uses_simple_calculation = false),
-      # Trait(name = "Brawl", trait_type = self.trait_types[1], category = Trait.category_options.physical, uses_simple_calculation = false),
-      # Trait(name = "Drive", trait_type = self.trait_types[1], category = Trait.category_options.physical, uses_simple_calculation = false),
-      # Trait(name = "Firearms", trait_type = self.trait_types[1], category = Trait.category_options.physical, uses_simple_calculation = false),
-      # Trait(name = "Larceny", trait_type = self.trait_types[1], category = Trait.category_options.physical, uses_simple_calculation = false),
-      # Trait(name = "Stealth", trait_type = self.trait_types[1], category = Trait.category_options.physical, uses_simple_calculation = false),
-      # Trait(name = "Weaponry", trait_type = self.trait_types[1], category = Trait.category_options.physical, uses_simple_calculation = false),
-      # Trait(name = "Animal Ken", trait_type = self.trait_types[1], category = Trait.category_options.social, uses_simple_calculation = false),
-      # Trait(name = "Empathy", trait_type = self.trait_types[1], category = Trait.category_options.social, uses_simple_calculation = false),
-      # Trait(name = "Expression", trait_type = self.trait_types[1], category = Trait.category_options.social, uses_simple_calculation = false),
-      # Trait(name = "Intimidation", trait_type = self.trait_types[1], category = Trait.category_options.social, uses_simple_calculation = false),
-      # Trait(name = "Persuasion", trait_type = self.trait_types[1], category = Trait.category_options.social, uses_simple_calculation = false),
-      # Trait(name = "socialize", trait_type = self.trait_types[1], category = Trait.category_options.social, uses_simple_calculation = false),
-      # Trait(name = "Streetwise", trait_type = self.trait_types[1], category = Trait.category_options.social, uses_simple_calculation = false),
-      # Trait(name = "Subterfuge", trait_type = self.trait_types[1], category = Trait.category_options.social, uses_simple_calculation = false)
-    # ]
-            
-  # def setup_trait_types(self):
-    # self.trait_types = [ 
-      # trait_type(name = "Attribute", default_xp_cost_per_dot = 5),
-      # trait_type(name = "Skill", default_xp_cost_per_dot = 3),
-      # trait_type(name = "Advantage", default_xp_cost_per_dot = 8),
-      # trait_type(name = "Merit", default_xp_cost_per_dot = 2)
-    # ]
-    
+
 class Geist(Game):
   keys = models.ManyToManyField(Trait, related_name='keys', limit_choices_to={'trait_type__name':'Key'})
 
