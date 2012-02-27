8609810f1493b80f7107159d0957e561988c27e8
Melissa Avery-Weir <averymd@irrsinn.net>
2012-02-26 20:11:16 -0500

    Adding in files related to the data entry.
    Signed-off-by: Melissa Avery-Weir <averymd@irrsinn.net>

---
+++ old_models/geist.py
@@ -0,0 +1,49 @@
+from game import Game
+from django.db import models
+
+
+    # self.name = 'Geist'
+    # self.subrace_name = 'Threshold';
+    # self.faction_name = 'Archetype';
+    # self.energy_name = 'Psyche';
+    # self.morality = Trait(name = 'Synergy');
+    # self.trait_types.append(trait_type(name = 'Keys', default_xp_cost_per_dot = 10 ));
+    # setup_keys();
+    # setup_thresholds();
+    # setup_archetypes();
+    
+  # def setup_archetypes(self):
+    # self.factions = [
+      # Faction(name = "Advocate"),
+      # Faction(name = "Bonepicker"),
+      # Faction(name = "Celebrant"),
+      # Faction(name = "Gatekeeper"),
+      # Faction(name = "Mourner"),
+      # Faction(name = "Necromancer"),
+      # Faction(name = "Pilgrim"),
+      # Faction(name = "Reaper")
+    # ]
+    
+  # def setup_thresholds(self) {
+    # self.subraces = [
+      # Subrace(name = "Torn"),
+      # Subrace(name = "Silent"),
+      # Subrace(name = "Prey"),
+      # Subrace(name = "Stricken"),
+      # Subrace(name = "Forgotten")
+    # ]
+    
+  # def setup_keys(self):
+    # self.keys = [
+      # Trait(name = "Cold Wind", specific_dots = [1], trait_type = self.trait_types[4], uses_simple_calculation=true),
+      # Trait(name = "Grace-dirt", specific_dots = [1], trait_type = self.trait_types[4], uses_simple_calculation=true),
+      # Trait(name = "Pyre Flame", specific_dots = [1], trait_type = self.trait_types[4], uses_simple_calculation=true),
+      # Trait(name = "Tear-stained", specific_dots = [1], trait_type = self.trait_types[4], uses_simple_calculation=true),
+      # Trait(name = "Industrial", specific_dots = [1], trait_type = self.trait_types[4], uses_simple_calculation=true),
+      # Trait(name = "Passion", specific_dots = [1], trait_type = self.trait_types[4], uses_simple_calculation=true),
+      # Trait(name = "Phantasmal", specific_dots = [1], trait_type = self.trait_types[4], uses_simple_calculation=true),
+      # Trait(name = "Primeval", specific_dots = [1], trait_type = self.trait_types[4], uses_simple_calculation=true),
+      # Trait(name = "Stigmata", specific_dots = [1], trait_type = self.trait_types[4], uses_simple_calculation=true),
+      # Trait(name = "Stillness", specific_dots = [1], trait_type = self.trait_types[4], uses_simple_calculation=true),
+      # Trait(name = "Stygian", specific_dots = [1], trait_type = self.trait_types[4], uses_simple_calculation=true)
+    # ]
