e678494a13d7e05e2391303024cc2131d77e4a5d
Melissa Avery-Weir <averymd@irrsinn.net>
2013-04-18 23:36:38 -0400

    Down through attributes, minus subrace, are working. Skills are claiming undefinted array, which doesn't make sense.
    Signed-off-by: Melissa Avery-Weir <averymd@irrsinn.net>

---
+++ serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from game_manager.models import Trait, Game, Geist, Faction, Subrace, Power
+
+class FactionSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Faction
+    fields = ('id', 'name')
+
+class TraitSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Trait
+    fields = ('id', 'name', 'category', 'use', 'specific_dots', 'trait_type', 'uses_simple_calculation', 'custom_xp_per_dot')
+  
+class GeistSerializer(serializers.ModelSerializer):
+  morality = serializers.RelatedField()
+  power_level_trait = TraitSerializer()
+  keys = TraitSerializer(many=True)
+  merits = TraitSerializer(many=True)
+  skills = TraitSerializer(many=True)
+  
+  class Meta:
+    model = Geist
+    fields = ('id', 'name', 'subrace_name', 'faction_name', 'energy_name', 'power_level_trait', 'keys', 'morality', 'merits', 'skills')
--- views.py
+++ views.py
@@ -1,1 +1,27 @@
-# Create your views here.
+from rest_framework import status
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from game_manager.models import Trait, Game, Geist, Faction, Subrace, Power
+from game_manager.serializers import FactionSerializer, GeistSerializer
+
+class FactionList(APIView):
+  """
+  List all factions in a game.
+  """
+  def get(self, request, game_id, format=None):
+    factions = Faction.objects.filter(game__id=game_id)
+    serializer = FactionSerializer(factions, many=True)
+    return Response(serializer.data)
+    
+class GameDetail(APIView):
+  """
+  Get the details for a game type.
+  """
+  def get(self, request, game_name, game_id, format=None):
+    if game_name.lower() == 'geist':
+      game = Geist.objects.select_related().get(pk=game_id)
+      serializer = GeistSerializer(game, many=False)
+    else:
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.data)
