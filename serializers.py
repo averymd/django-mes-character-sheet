e69d923611a502cc37fbf271f4f5dc22947861c4
Melissa Avery-Weir <averymd@irrsinn.net>
2013-06-24 10:50:47 -0400

    Character_manager app added.
    Signed-off-by: Melissa Avery-Weir <averymd@irrsinn.net>

--- serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from character_manager.models import Character
-#from game_manager.models import Trait, Game, Geist, Faction, Subrace
-
-class CharacterSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Character
-    fields = ('name', 'concept', 'age', 'dob', 'virtue', 'vice', 'mc_level_at_creation')
