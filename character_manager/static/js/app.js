203c9df78d4a8ea95f7db2b336b300b9405ae14a
Gregory Avery-Weir <Gregory.Weir@gmail.com>
2013-07-09 17:34:49 -0400

    Added more ember views

--- character_manager/static/js/app.js
+++ character_manager/static/js/app.js
@@ -3,12 +3,39 @@ CharonSheet = Ember.Application.create({
 });
 
 CharonSheet.Router.map(function() {
-  this.resource('tabletop', { path: '/'
+  this.resource('tabletop', { path: '/'});
 });
-});
+
+CharonSheet.virtues = ['Charity', 'Faith', 'Fortitude', 'Hope', 'Justice', 'Prudence', 'Temperance']; //TODO: Take these from Game and keep them in the model?
+CharonSheet.vices = ['Charity', 'Faith', 'Fortitude', 'Hope', 'Justice', 'Prudence', 'Temperance']; //TODO: Take these from Game and keep them in the model?
+
+CharonSheet.skills = {
+	mental: ['Academics', 'Computer', 'Crafts', 'Investigation', 'Medicine', 'Occult', 'Politics', 'Science'],
+	physical: ['Athletics', 'Brawl', 'Drive', 'Firearms', 'Larceny', 'Stealth', 'Survival', 'Weaponry'],
+	social: ['Animal Ken', 'Empathy', 'Expression', 'Intimidation', 'Persuasion', 'Socialize', 'Streetwise', 'Subterfuge']
+};
 
 CharonSheet.TabletopRoute = Ember.Route.extend({
   model: function () {
     return CharonSheet.character.find();
   }
+});
+
+CharonSheet.TraitView = Ember.View.extend({
+	templateName: 'trait',
+	idPrefix: function(){
+		console.info(this.get("trait").toLowerCase().replace(" ",""));
+		return this.get("trait").toLowerCase().replace(" ",""); //We'll wanna replace this with the trait's actual ID.
+	}.property('trait')
+});
+
+Ember.Handlebars.registerBoundHelper('dots', function() {
+	var ret = '';
+	var n = arguments[1] || 5;
+
+	for(var i=1; i<=n; i++)
+	{
+		ret += '<input type="checkbox" id="' + arguments[0] + '-attr-'+ i + '"/><label for="' + arguments[0] +'-attr-'+ i + '"></label>';
+	}
+	return new Handlebars.SafeString(ret);
 });
--- character_manager/static/js/models/character.js
+++ character_manager/static/js/models/character.js
@@ -1,10 +1,22 @@
 CharonSheet.character = DS.Model.extend({
-	name: DS.attr('string')
+	name: DS.attr('string'),
+	concept: DS.attr('string'),
+  age: DS.attr('string'),
+  dob: DS.attr('string'), //Make this a date?
+  virtue: DS.attr('number'), //TODO: Replace this with a connection to the game's virtue array!
+  vice: DS.attr('number'), //TODO: Replace this with a connection to the game's virtue array!
+  mc_level_at_creation: DS.attr('number')
 })
 
 CharonSheet.character.FIXTURES = [
 {
 	id: 0,
-	name: 'Archibald Fortinbras Barrayan'
+	name: 'Archibald Fortinbras Barrayan',
+	concept: 'Dead socialite',
+	age: '30',
+	dob: '1/1/1983',
+	virtue: 1,
+	vice: 1,
+	mc_level_at_creation: 1
 }
 ]
